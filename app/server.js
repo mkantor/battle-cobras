6f19a8f5048e1a84e36e19916bee93c1ccac742e
Jason Khoe <jkhoe@JasonKhoe-Mac.corp.kabam.com>
2013-06-13 15:18:18 -0700

    Cleaned up server.js

--- app/server.js
+++ app/server.js
@@ -42,24 +42,10 @@ var app = http.createServer(function(req, res) {
   });
 }).listen(80);
 
-/*fs.readFile('./client.html', function (err, html) {
-  if (err) {
-    throw err;
-  }
-
-  var app = http.createServer(function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(html);
-      res.end();
+var io = socketIO.listen(app);
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
-  app.listen(80);
-  console.log('Server listening on port 80');
-*/
-  var io = socketIO.listen(app);
-  io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
-  });
-//});
+});
