00c05881034a11088383e6803d3d6a39720c974a
Matt Kantor <mkantor@kabam.com>
2013-06-13 16:54:16 -0700

    Allow arrow keys for movement.

--- app/script.js
+++ app/script.js
@@ -15,4 +15,25 @@ $(document).ready(function() {
   $('#controls .move').click(function(event) {
     socketIO.emit('move', $(this).data());
   });
+
+  // Allow using arrow keys for movement.
+  $(document).keydown(function(event) {
+    switch(event.which) {
+      case 37: // left arrow
+        socketIO.emit('move', { direction: 'left' });
+      break;
+
+      case 38: // up arrow
+        socketIO.emit('move', { direction: 'up' });
+      break;
+
+      case 39: // right arrow
+        socketIO.emit('move', { direction: 'right' });
+      break;
+
+      case 40: // down arrow
+        socketIO.emit('move', { direction: 'down' });
+      break;
+    }
+  });
 });
