74953075e5731dbf9ad2e2b5f91b839db276e82d
Matt Kantor <mkantor@kabam.com>
2013-06-13 14:13:30 -0700

    Socket.io hello world.

--- app/server.js
+++ app/server.js
@@ -1,14 +1,25 @@
 var http = require('http');
-var fs   = require('fs');
+var fs = require('fs');
+var socketIO = require('socket.io');
 
 fs.readFile('./client.html', function (err, html) {
   if (err) {
     throw err;
   }
-  http.createServer(function (req, res) {
+
+  var app = http.createServer(function (req, res) {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(html);
       res.end();
-  }).listen(80);
+  });
+  app.listen(80);
+  console.log('Server listening on port 80');
+
+  var io = socketIO.listen(app);
+  io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+  });
 });
-console.log('Server running on port 3000');
