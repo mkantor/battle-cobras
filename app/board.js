ac7e846530b1e0e4dc1fdc5ce0a589064766e4ab
Jason Khoe <jkhoe@JasonKhoe-Mac.corp.kabam.com>
2013-06-13 16:43:46 -0700

    Fixed cell indexing

--- app/board.js
+++ app/board.js
@@ -8,7 +8,7 @@ var Board = {
     $("#grid").css({"height": HEIGHT*50, "width": WIDTH*50 });
     for (var i = 0; i < WIDTH*HEIGHT; i++) {
       grid = grid + '<div id="sq-' +
-         String(Math.floor(i/HEIGHT)) + '-' +
+         String(Math.floor(i/WIDTH)) + '-' +
          String(i%WIDTH) + '" class="sq"></div>';
     }
     $("#grid").append(grid);
