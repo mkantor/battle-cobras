50e0c399236ae7e83596865ad4434bd333a35a77
Jason Khoe <jkhoe@JasonKhoe-Mac.corp.kabam.com>
2013-06-13 18:21:51 -0700

    Made starting position random

--- app/server.js
+++ app/server.js
@@ -59,7 +59,8 @@ var io = socketIO.listen(app);
 io.sockets.on('connection', function (socket) {
   // Add the new player.
   players[socket.id] = {
-    position: { x: 0, y: 0 }, // TODO: Decide where to spawn based on world state (away from other players?).
+    position: { x: Math.floor((Math.random()*board.width)+1),
+                y: Math.floor((Math.random()*board.width)+1) }, // TODO: Decide where to spawn based on world state (away from other players?).
     team: (function() {
       var teams = {};
       for (var player in players) {
